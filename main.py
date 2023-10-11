d83f259a4fca24033852f17f5e18e50386f5ea11
Jaden Bartlett <Jaden.b123@outlook.com>
2023-10-11 08:54:18 +0100

    projectiles are now deleted when leaving the screen

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Player:
     def __init__(self):
         self.x = 300
         self.y = 500
-        # this is the size of the player
+        # this is the starting position of the player
 
     def move(self, direction1, shift):
         # direction1 is the direction that is told to it from the code
@@ -22,40 +22,40 @@ class Player:
         # checks the current direction
         if direction1 == "up":
             # checks if at top of screen
-            if self.y == 3:
+            if self.y <= 3:
                 self.y = 3
             # if it is at the top of the screen it will stay still
             else:
-                if shift == True:
+                if shift:
                     self.y -= 0.08
                 else:
                     self.y -= 0.15
         elif direction1 == "down":
-            if self.y == 600 - 3:
+            if self.y >= 600 - 3:
                 self.y = 600 - 3
             else:
-                if shift == True:
+                if shift:
                     self.y += 0.08
                 else:
                     self.y += 0.15
         if direction1 == "left":
-            if self.x == 3:
+            if self.x <= 3:
                 self.x = 3
             else:
-                if shift == True:
+                if shift:
                     self.x -= 0.08
                 else:
                     self.x -= 0.15
         elif direction1 == "right":
-            if self.x == 600 - 3:
+            if self.x >= 600 - 3:
                 self.x = 600 - 3
             else:
-                if shift == True:
+                if shift:
                     self.x += 0.08
                 else:
                     self.x += 0.15
 
-    # draw the character
+    # draw the character and defines its size
     def display(self):
         pygame.draw.circle(screen, (255, 255, 255), (self.x, self.y), 3)
 
@@ -66,16 +66,19 @@ class projectile:
         self.y = y
         self.width = 10
         self.height = 10
+        self.rect = pygame.Rect(self.x,self.y,self.width,self.height)
 
     def display(self):
-        pygame.draw.rect(screen, (255, 0, 0), (self.x, self.y))
-        self.y -= 2
+        pygame.draw.rect(screen, (255, 0, 0), self.rect)
+        self.rect.y -= 1
 
 
 player1 = Player()
-
+Clock = pygame.time.Clock()
 screen.fill((0, 0, 0))
 player1.display()
+lastTime = 0
+firstTime = 0
 
 while True:
     # closes the game if the pygame window is closed
@@ -84,8 +87,8 @@ while True:
             pygame.quit()
             sys.exit()
     # checks if the shift is pressed and gives true or false from it
-    shiftpressed = pygame.key.get_pressed()
-    if shiftpressed[pygame.K_LSHIFT]:
+    shiftPressed = pygame.key.get_pressed()
+    if shiftPressed[pygame.K_LSHIFT]:
         shift = True
     else:
         shift = False
@@ -101,13 +104,36 @@ while True:
     if keystate[pygame.K_RIGHT]:
         player1.move("right", shift)
 
-
-    checkfire = pygame.key.get_pressed()
-    if checkfire[pygame.K_z]:
+    # checks if z is pressed if it is it will fire from the player
+    fire = False
+    time = False
+    checkFire = pygame.key.get_pressed()
+    if checkFire[pygame.K_z]:
+        firstTime = pygame.time.get_ticks()
         fire = True
 
-    if fire == True:
-        projectileArray.append(projectile())
+    if firstTime-lastTime>=20:
+        time = True
+
+
+    if fire and time:
+        lastTime = pygame.time.get_ticks()
+        projectileArray.append(projectile(player1.x, player1.y))
+        time = False
+
+    # redraws the screen
     screen.fill((0, 0, 0))
     player1.display()
+    for i in range(0, len(projectileArray)):
+        try:
+            if projectileArray[i].rect.y < 0:
+                del projectileArray[i]
+        except:
+            pass
+        try:
+            projectileArray[i].display()
+        except:
+            pass
+
     pygame.display.flip()
+
